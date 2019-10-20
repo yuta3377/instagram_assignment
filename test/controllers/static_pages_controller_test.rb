59a9dcab318ed2f68352ebb96f31be3ad1e731c4
EC2 Default User <ec2-user@ip-172-31-22-120.ap-southeast-1.compute.internal>
2019-10-20 13:13:47 +0000

    Add views for static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,21 +4,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
+    assert_select "title", "Home | Instagram Sample App"
   end
 
   test "should get help" do
     get help_path
     assert_response :success
+    assert_select "title", "Help | Instagram Sample App"
   end
   
   test "should get about" do
     get about_path
     assert_response :success
+    assert_select "title", "About | Instagram Sample App"
   end
   
   test "should get contact" do
     get contact_path
     assert_response :success
+    assert_select "title", "Contact | Instagram Sample App"
   end
   
 
