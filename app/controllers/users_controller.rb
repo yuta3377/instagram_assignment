7fc0f6f5115d0df35350aa40e0babbdefa0812f3
EC2 Default User <ec2-user@ip-172-31-22-120.ap-southeast-1.compute.internal>
2019-10-21 15:35:51 +0000

    Add flash in signup success

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to Instagram Sample App!"
       redirect_to @user
     else
       render 'new'
@@ -19,6 +20,7 @@ class UsersController < ApplicationController
   
   private
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
 end
