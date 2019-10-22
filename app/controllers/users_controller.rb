c8bfe80403a572907044ed19773a64053227b4bf
EC2 Default User <ec2-user@ip-172-31-22-120.ap-southeast-1.compute.internal>
2019-10-22 12:06:19 +0000

    Modify test for correct_user inspection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -21,11 +22,9 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
-  
+
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -42,8 +41,13 @@ class UsersController < ApplicationController
     
     def logged_in_user
       unless logged_in?
-        flash[:danger] = "Please log in"
+        flash[:danger] = "Please log in."
         redirect_to login_url
       end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
