2878218ecf4a68ef7fd55ed4b6a1a26a66a54a9b
EC2 Default User <ec2-user@ip-172-31-22-120.ap-southeast-1.compute.internal>
2019-10-22 05:25:44 +0000

    Add login after user resistration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to Instagram Sample App!"
       redirect_to @user
     else
