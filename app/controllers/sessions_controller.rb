71b58f974f7cbac8075a515a9274fb066695e4e8
EC2 Default User <ec2-user@ip-172-31-22-120.ap-southeast-1.compute.internal>
2019-10-22 05:13:05 +0000

    Add flash when login is successed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      flash[:success] = "Login is Successful"
       redirect_to user
     else
       flash.now[:danger] = "Invalid email/password combination"
